8c4e1ff39346fffed9d4c6ea9ec513f63e63ca67
Narantyo Maulana <olandeoxys@gmail.com>
2025-10-15 14:42:29 +0700

    [ADD] User Controller, user request and pagination

--- user-service/app/container.go
+++ user-service/app/container.go
@@ -6,11 +6,13 @@ import (
 	"micro-warehouse/user-service/controller"
 	"micro-warehouse/user-service/database"
 	"micro-warehouse/user-service/repository"
+	"micro-warehouse/user-service/service"
 	"micro-warehouse/user-service/usecase"
 )
 
 type Container struct {
 	RoleController controller.RoleControllerInterface
+	UserController controller.UserControllerInterface
 }
 
 func BuildContainer() *Container {
@@ -20,11 +22,21 @@ func BuildContainer() *Container {
 		log.Fatalf("Failed to connect to database: %v", err)
 	}
 
+	rabbitMQService, err := service.NewRabbitMQService(*config)
+	if err != nil {
+		log.Fatalf("Failed to connect to rabbitmq: %v", err)
+	}
+
 	roleRepo := repository.NewRoleRepository(db.DB)
 	roleUsecase := usecase.NewRoleUsecase(roleRepo)
 	roleController := controller.NewRoleController(roleUsecase)
 
+	userRepo := repository.NewUserRepository(db.DB)
+	userUsecase := usecase.NewUserUsecase(userRepo, rabbitMQService)
+	userController := controller.NewUserController(userUsecase)
+
 	return &Container{
 		RoleController: roleController,
+		UserController: userController,
 	}
 }
